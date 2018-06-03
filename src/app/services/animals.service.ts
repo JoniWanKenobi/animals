cf46690149c6d928d55c698db7b9a223279ed065
JoniWanKenobi <joniwankenoby@gmail.com>
2018-06-04 01:04:56 +0200

    removed http client

--- src/app/services/animals.service.ts
+++ src/app/services/animals.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpResponse } from '@angular/common/http';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/toPromise';
